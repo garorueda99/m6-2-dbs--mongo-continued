8866ef9d27de7140cf7acc374b895a2c668fb5bb
Andres Rueda <garo99@gmail.com>
2020-10-05 18:45:23 -0400

    start

--- src/components/Seat.js
+++ src/components/Seat.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import Tippy from '@tippy.js/react';
+import 'tippy.js/dist/tippy.css';
+import 'tippy.js/themes/material.css';
+import 'tippy.js/animations/scale-subtle.css';
 import VisuallyHidden from '@reach/visually-hidden';
 import seatImageSrc from '../assets/seat-available.svg';
 import { getRowName, getSeatNum, encodeSeatId } from '../helpers';
@@ -19,7 +22,16 @@ const Seat = ({ rowIndex, seatIndex, width, height, price, status }) => {
   const seatId = encodeSeatId(rowIndex, seatIndex);
 
   return (
-    <Tippy content={`Row ${rowName}, Seat ${seatNum} – $${price}`}>
+    <TippyF
+      content={`Row ${rowName}, Seat ${seatNum} – $${price}`}
+      placement="top"
+      animation="scale-subtle"
+      theme="material"
+      arrow={true}
+      duration={200}
+      delay={[400, 0]}
+      distance={8}
+    >
       <Wrapper
         disabled={status === 'unavailable'}
         onClick={() => {
@@ -29,9 +41,9 @@ const Seat = ({ rowIndex, seatIndex, width, height, price, status }) => {
         <VisuallyHidden>
           Seat number {seatNum} in Row {rowName}
         </VisuallyHidden>
-        <img src={seatImageSrc} alt='' style={{ width, height }} />
+        <img src={seatImageSrc} alt="" style={{ width, height }} />
       </Wrapper>
-    </Tippy>
+    </TippyF>
   );
 };
 
@@ -43,4 +55,24 @@ const Wrapper = styled(UnstyledButton)`
   }
 `;
 
+const TippyF = styled(Tippy)`
+  background-color: gray;
+  border-radius: 6px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &:after {
+    content: '';
+    position: absolute;
+    top: 98%;
+    left: 0;
+    right: 0;
+    margin: 0 auto;
+    width: 0;
+    height: 0;
+    border-top: solid 15px gray;
+    border-left: solid 15px transparent;
+    border-right: solid 15px transparent;
+  }
+`;
 export default Seat;
