67d1ea0652c186d33b47252429bc43dcfbb30c47
Andres Rueda <garo99@gmail.com>
2020-10-06 15:20:02 -0400

    postPurchanse

--- server/handlers.js
+++ server/handlers.js
@@ -9,14 +9,6 @@ const getSeats = async (req, res) => {
   const { start = 0, limit = 96 } = req.query;
   const [startInt, limitInt] = Object.values({ start, limit }).map(Number);
 
-  let state;
-
-  // if (!state) {
-  //   state = {
-  //     bookedSeats: randomlyBookSeats(30),
-  //   };
-  // }
-
   try {
     const client = await MongoClient(MONGO_URI, options);
     await client.connect();
@@ -42,4 +34,47 @@ const getSeats = async (req, res) => {
   }
 };
 
-module.exports = { getSeats };
+const postPurchase = async (req, res) => {
+  const { seatId, creditCard, expiration } = req.body;
+
+  if (!state) {
+    state = {
+      bookedSeats: randomlyBookSeats(30),
+    };
+  }
+
+  await delay(Math.random() * 3000);
+
+  const isAlreadyBooked = !!state.bookedSeats[seatId];
+  if (isAlreadyBooked) {
+    return res.status(400).json({
+      message: 'This seat has already been booked!',
+    });
+  }
+
+  if (!creditCard || !expiration) {
+    return res.status(400).json({
+      status: 400,
+      message: 'Please provide credit card information!',
+    });
+  }
+
+  if (lastBookingAttemptSucceeded) {
+    lastBookingAttemptSucceeded = !lastBookingAttemptSucceeded;
+
+    return res.status(500).json({
+      message: 'An unknown error has occurred. Please try your request again.',
+    });
+  }
+
+  lastBookingAttemptSucceeded = !lastBookingAttemptSucceeded;
+
+  state.bookedSeats[seatId] = true;
+
+  return res.status(200).json({
+    status: 200,
+    success: true,
+  });
+};
+
+module.exports = { getSeats, postPurchase };
